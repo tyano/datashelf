e8bef12664df12d3dae2678a3032f7e7b73b051b
Tsutomu YANO <tsutomuyano@shelfinc.com>
2022-02-15 19:32:09 +0900

    Implemented ObjectStore

---
+++ src/main/datashelf/key-range.cljs
@@ -0,0 +1,62 @@
+(ns datashelf.key-range)
+
+(defrecord KeyRange [key-range])
+
+(defn make-key-range-instance
+  [key-range]
+  (->KeyRange key-range))
+
+(defn key-range?
+  [maybe-key-range]
+  (and (some? maybe-key-range) 
+       (instance? KeyRange maybe-key-range)))
+
+(defn to-js
+  [{:keys [key-range]}]
+  key-range)
+
+(defn lower-bound
+  ([lower open]
+   (make-key-range-instance (js/IDBKeyRange.lowerBound lower open)))
+  ([lower]
+   (make-key-range-instance (js/IDBKeyRange.lowerBound lower))))
+
+(defn upper-bound
+  ([upper open]
+   (make-key-range-instance (js/IDBKeyRange.upperBound upper open)))
+  ([upper]
+   (make-key-range-instance (js/IDBKeyRange.upperBound upper))))
+
+(defn only
+  [value]
+  (make-key-range-instance (js/IDBKeyRange.only value)))
+
+(defn bound
+  ([lower upper lowerOpen upperOpen]
+   (make-key-range-instance (js/IDBKeyRange.bound lower upper lowerOpen upperOpen)))
+  
+  ([lower upper lowerOpen]
+   (make-key-range-instance (js/IDBKeyRange.bound lower upper lowerOpen)))
+  
+  ([lower upper]
+   (make-key-range-instance (js/IDBKeyRange.bound lower upper))))
+
+(defn lower
+  [{:keys [key-range]}]
+  {:keys [key-range]}
+  (.-lower key-range))
+
+(defn upper
+  [{:keys [key-range]}]
+  {:keys [key-range]}
+  (.-upper key-range))
+
+(defn lower-open
+  [{:keys [key-range]}]
+  {:keys [key-range]}
+  (.-lowerOpen key-range))
+
+(defn upper-open
+  [{:keys [key-range]}]
+  {:keys [key-range]}
+  (.-upperOpen key-range))
--- src/main/datashelf/object_store.cljs
+++ src/main/datashelf/object_store.cljs
@@ -1,24 +1,15 @@
 (ns datashelf.object-store
+  (:refer-clojure :exclude [count get])
   (:require [datashelf.lang.core :refer [keep-keys to-camel-case]]
             [datashelf.lang.string-list :refer [to-vector]]
             [datashelf.database :refer [make-transaction-instance]]
             [clojure.core.async :refer [promise-chan go >! close!]]
-            [databox.core :as databox]))
+            [databox.core :as databox]
+            [datashelf.key-range :refer [key-range?] :as key-range]))
 
 (defn make-index-instance
   [js-index]
   {:index js-index})
-
-(defn create-index
-  [{:keys [object-store]} key-path & [options]]
-  {:pre [object-store key-path]}
-  (let [index-data (if options
-                     (let [js-opts (-> options
-                                       (keep-keys #(-> % name to-camel-case))
-                                       (clj->js))]
-                       (.createIndex object-store key-path js-opts))
-                     (.createIndex object-store key-path))]
-    (make-index-instance index-data)))
 
 (defn index-names
   [{:keys [object-store]}]
@@ -45,25 +36,210 @@
   {:pre [object-store]}
   (.-autoIncrement object-store))
 
+(defn setup-request-handlers
+  [request ch & [result-fn]]
+  (set! (.-onsuccess request)
+        (fn [e]
+          (let [result (.-result request)
+                result (if result-fn (result-fn result) result)]
+            (go
+              (>! ch (databox/success result))
+              (close! ch)))))
+
+  (set! (.-onerror request)
+        (fn [_]
+          (let [error (.-error request)]
+            (go
+              (>! ch (databox/failure error))
+              (close! ch))))))
+
+(defn- flatten-map
+  [m]
+  (-> m seq flatten))
+
 (defn add
-  [{:keys [object-store]} value key]
+  ([{:keys [object-store]} value key options]
+   {:pre [object-store]}
+   (let [ch (promise-chan)
+         data    (apply clj->js value (flatten-map options))
+         request (if key
+                   (.add object-store data key)
+                   (.add object-store data))]
+     (setup-request-handlers request ch)
+     ch))
+  
+  ([object-store-instance value options]
+   (add object-store-instance value nil options)))
+
+(defn clear
+  [{:keys [object-store]}]
   {:pre [object-store]}
   (let [ch (promise-chan)
-        request (if key
-                  (.add object-store value key)
-                  (.add object-store value))]
-    (set! (.-onsuccess request)
-          (fn [_]
-            (let [result (.-result request)]
-              (go
-                (>! ch (databox/success result))
-                (close! ch)))))
-    
-    (set! (.-onerror request)
-          (fn [_]
-            (let [error (.-error request)]
-              (go
-                (>! ch (databox/failure error))
-                (close! ch)))))
-    
+        request (.clear object-store)]
+    (setup-request-handlers request ch)
     ch))
+
+(defn- resolve-key-range
+  [maybe-key-range]
+  (if (key-range? maybe-key-range) (key-range/to-js maybe-key-range) maybe-key-range))
+
+(defn count
+  ([{:keys [object-store]} query]
+   {:pre [object-store]}
+   (let [ch (promise-chan)
+         request (if-let [range (resolve-key-range query)]
+                   (.count object-store range)
+                   (.count object-store))]
+     (setup-request-handlers request ch)
+     ch))
+  
+  ([instance]
+   (count instance nil)))
+
+(defn create-index
+  [{:keys [object-store]} key-path & [options]]
+  {:pre [object-store key-path]}
+  (let [index-data (if options
+                     (let [js-opts (-> options
+                                       (keep-keys #(-> % name to-camel-case))
+                                       (clj->js))]
+                       (.createIndex object-store key-path js-opts))
+                     (.createIndex object-store key-path))]
+    (make-index-instance index-data)))
+
+(defn delete
+  [{:keys [object-store]} key]
+  {:pre [object-store key]}
+  (let [ch (promise-chan)
+        request (.delete object-store (resolve-key-range key))]
+    (setup-request-handlers request ch)
+    ch))
+
+(defn delete-index
+  [{:keys [object-store]} index-name]
+  {:pre [object-store index-name]}
+  (.deleteIndex object-store index-name))
+
+(defn get
+  [{:keys [object-store]} key]
+  {:pre [object-store key]}
+  (let [ch (promise-chan)
+        request (.get object-store key)]
+    (setup-request-handlers request ch #(js->clj %))
+    ch))
+
+(defn get-key
+  [{:keys [object-store]} key options]
+  {:pre [object-store key]}
+  (let [ch (promise-chan)
+        request (.getKey object-store (resolve-key-range key))]
+    (setup-request-handlers request ch #(apply js->clj % (flatten-map options)))
+    ch))
+
+(defn get-all
+  ([{:keys [object-store]} query count options]
+   {:pre [object-store  
+          (if count (or (zero? count) (pos-int? count)) true)]}
+   (let [ch (promise-chan)
+         range   (resolve-key-range query)
+         request (cond
+                   (and range count)
+                   (.getAll object-store range count)
+                   
+                   range
+                   (.getAll object-store range)
+                   
+                   :else
+                   (.getAll object-store))]
+     (setup-request-handlers request ch #(apply js->clj % (flatten-map options)))
+     ch))
+  
+  ([instance query options]
+   (get-all instance query nil options))
+  
+  ([instance options]
+   (get-all instance nil options)))
+
+(defn get-all-keys
+  ([{:keys [object-store]} query count options]
+   {:pre [object-store 
+          (if count (or (zero? count) (pos-int? count)) true)]}
+   (let [ch (promise-chan)
+         range   (resolve-key-range query)
+         request (cond
+                   (and range count)
+                   (.getAllKeys object-store range count)
+                   
+                   range
+                   (.getAllKeys object-store range)
+                   
+                   :else
+                   (.getAllKeys object-store))]
+     (setup-request-handlers request ch #(apply js->clj % (flatten-map options)))
+     ch))
+  
+  ([instance query options]
+   (get-all-keys instance query nil options))
+  
+  ([instance options]
+   (get-all-keys instance nil options)))
+
+(defn index
+  [{:keys [object-store]} index-name]
+  {:pre [object-store index-name]}
+  (make-index-instance (.index object-store index-name)))
+
+(defn- to-cursor
+  [js-cursor]
+  ;;TODO
+  )
+
+(defn open-cursor
+  [{:keys [object-store]} query direction]
+  {:pre [object-store (if direction (#{:next :nextunique :prev :prevunique} direction) true)]}
+  (let [ch (promise-chan)
+        range (resolve-key-range query)
+        request (cond
+                  (and range direction)
+                  (.openCursor object-store range (name direction))
+                  
+                  range
+                  (.openCursor object-store range)
+                  
+                  :else
+                  (.openCursor object-store))]
+    (setup-request-handlers request ch to-cursor)
+    ch))
+
+(defn open-key-cursor
+  [{:keys [object-store]} query direction]
+  {:pre [object-store (if direction (#{:next :nextunique :prev :prevunique} direction) true)]}
+  (let [ch (promise-chan)
+        range (resolve-key-range query)
+        request (cond
+                  (and range direction)
+                  (.openKeyCursor object-store range (name direction))
+
+                  range
+                  (.openKeyCursor object-store range)
+
+                  :else
+                  (.openKeyCursor object-store))]
+    (setup-request-handlers request ch to-cursor)
+    ch))
+
+(defn put
+  ([{:keys [object-store]} item key options]
+   {:pre [object-store item]}
+   (let [ch (promise-chan)
+         clj->js-options (select-keys options [:keyword-fn])
+         js->clj-options (select-keys options [:keywordize-keys])
+         data    (apply clj->js item (flatten-map clj->js-options))
+         request (if (some? key)
+                   (.put object-store data key)
+                   (.put object-store data))]
+     (setup-request-handlers request ch #(apply js->clj % (flatten-map js->clj-options)))
+     ch))
+  
+  ([instance item options]
+   (put instance item nil options)))
