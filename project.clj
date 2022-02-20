239e660c631996f587d79078b1d2fbb381b013b1
Tsutomu YANO <tsutomuyano@shelfinc.com>
2022-02-20 17:42:31 +0900

    Added project.clj and moved dependencies to project.clj

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject datashelf "1.0-SNAPSHOT"
+  :description "CLJS library for IndexedDB of Javascript"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths
+  ["src/dev"
+   "src/main"
+   "src/test"]
+
+  :dependencies
+  [[thheller/shadow-cljs "2.17.3"]
+   [databox "0.2.0-SNAPSHOT"]])
