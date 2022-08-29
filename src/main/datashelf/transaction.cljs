11c64c0cef82fc71ba95dac6125edfda09f5a8b7
Tsutomu YANO <tsutomuyano@shelfinc.com>
2022-08-29 22:24:44 +0900

    Added 'open-event-channel' fn to transaction
    Promises returned from add and put method of
    objectStore sometimes never be called, because
    the completion of an transaction should be checked
    through events of transaction objects.
    
    open-event-channel returns an channel which
    returns an success boxed object if the 'complete'
    event is called on transaction object,
    and returns an failure boxed object if the 'abort'
    or 'error' event occurred.
    
    Users can check the failure box if it is 'abort' or
    'error' by checking the :type field of a returned
    map data.

--- src/main/datashelf/transaction.cljs
+++ src/main/datashelf/transaction.cljs
@@ -1,7 +1,9 @@
 (ns datashelf.transaction
   (:require [datashelf.database.instance :refer [make-db-instance]]
             [datashelf.lang.string-list :refer [to-vector]]
-            [datashelf.object-store.instance :refer [make-object-store-instance]]))
+            [datashelf.object-store.instance :refer [make-object-store-instance]]
+            [clojure.core.async :refer [go >! chan close!]]
+            [databox.core :as databox]))
 
 (defn make-transaction-instance
   [js-tx]
@@ -62,5 +64,13 @@
 (defn onerror
   [{:keys [transaction] :as tx} error-fn]
   {:pre [transaction error-fn (fn? error-fn)]}
-  (set! (.-onabort transaction) (fn [e] (error-fn e tx)))
+  (set! (.-onerror transaction) (fn [e] (error-fn e tx)))
   tx)
+
+(defn open-event-channel
+  [tx]
+  (let [tx-ch (chan)]
+    (oncomplete tx (fn [e _] (go (>! tx-ch (databox/success {:type :completed :event e})) (close! tx-ch))))
+    (onabort tx (fn [e _] (go (>! tx-ch (databox/failure {:type :aborted :event e})) (close! tx-ch))))
+    (onerror tx (fn [e _] (go (>! tx-ch (databox/failure {:type :error :event e})) (close! tx-ch))))
+    tx-ch))
